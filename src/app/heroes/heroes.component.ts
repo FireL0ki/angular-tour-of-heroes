56a588e3c4b71f05d9cf25ed1b229549d4cd78ac
Sif Oberon <muell758@d.umn.edu>
2023-10-24 20:07:48 -0500

    import OnInit, import MessageService, build constructor with heroService & messageService, build ngOnInit() method, fix formatting

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero'
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 // Parent component of HeroDetailComponent. 
 // This component sends the user selected hero for display to the Hero Details component.
@@ -10,29 +12,31 @@ import { HeroService } from '../hero.service';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
-
-  heroes: Hero[] = [];
+export class HeroesComponent implements OnInit {
   
   selectedHero?: Hero;
 
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
+  heroes: Hero[] = [];
 
-  // an RxJS (Reactive Extensions for JavaScript) method; calls the getHeroes function from the heroService module. 
-  // The subscribe() function listens to the observable and passes the emitted data to the callback function (heroes => this.heroes = heroes)
-  // The arrow function takes the parameter heroes, and when the observable emits data 'heroes' (the heroes list), assigns the data 'heroes' to the this.heroes class.
-  getHeroes(): void {
-    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes)
-  }
+    // constructor should be limited to wiring parameters to properties. It should not 'do anything' (ex. call a function, etc.)
+    constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   // Angular initialization function, runs on init.
   ngOnInit(): void {
     this.getHeroes();
   }
 
-  // constructor should be limited to wiring parameters to properties. It should not 'do anything' (ex. call a function, etc.)
-  constructor(private heroService: HeroService) {} // todo: Here?
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    this.messageService.add('HeroesComponent: Selected hero id=${hero.id}');
+  }
+
+  // an RxJS (Reactive Extensions for JavaScript) method; calls the getHeroes function from the heroService module. 
+  // The subscribe() function listens to the observable and passes the emitted data to the callback function (heroes => this.heroes = heroes)
+  // The arrow function takes the parameter heroes, and when the observable emits data 'heroes' (the heroes list), assigns the data 'heroes' to the this.heroes class.
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
 
 }
