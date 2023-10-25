4de0c263cbe8f113dd461a09ff49a2bcf70d4f3b
Sif Oberon <muell758@d.umn.edu>
2023-10-24 20:06:06 -0500

    import MessageService, add messageService to getHeroes(): method, add messageService to constructor

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
 import { Observable } from 'rxjs';
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,9 @@ export class HeroService {
   // Uses an Observable object-- to return objects for asynchronous fetching of data
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
     return heroes;
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
