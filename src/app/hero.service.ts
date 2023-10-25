5404237fb9ff5fef25c0a153f054f144b019dd4d
Sif Oberon <muell758@d.umn.edu>
2023-10-24 19:49:33 -0500

    rebuild getHeroes(): using Observable class for asynchronous data retrieval

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,14 +1,17 @@
 import { Injectable } from '@angular/core';
 import { Hero } from './hero'
 import { HEROES } from './mock-heroes'
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
-  
-  getHeroes(): Hero[] {
-    return HEROES;
+
+  // Uses an Observable object-- to return objects for asynchronous fetching of data
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    return heroes;
   }
 
   constructor() { }
