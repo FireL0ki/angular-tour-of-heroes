5aa68a31b8265ed0599b43e1784622d6b88e032f
Sif Oberon <muell758@d.umn.edu>
2023-10-26 19:45:30 -0500

    fix formatting, build constructor, call getHero() function

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -2,15 +2,14 @@ import { Injectable } from '@angular/core';
 
 import { Observable, of } from 'rxjs';
 
-import { Hero } from './hero'
-import { HEROES } from './mock-heroes'
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class HeroService {
 
+  constructor(private messageService: MessageService) { }
   // Uses an Observable object-- to return objects for asynchronous fetching of data
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
@@ -18,5 +17,10 @@ export class HeroService {
     return heroes;
   }
 
-  constructor(private messageService: MessageService) { }
+  getHero(id: number): Observable<Hero> {
+    // Currently assumes a hero with the specified `id` always exists.
+    const hero = HEROES.find(h => h.id === id)!;
+    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    return of(hero);
+  }
 }
